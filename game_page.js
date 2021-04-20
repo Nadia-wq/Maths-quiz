2043e2586741eeabb2d5a6d20ee9ae6491f64c95
Nadia-wq <72089749+Nadia-wq@users.noreply.github.com>
2021-04-20 19:21:04 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,62 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "question turn- " + player1_name;
+document.getElementById("player_answer").innerHTML = "answer turn- " + player2_name;
+
+function send() {
+    number_1=document.getElementById("number_1").value;
+    number_2=document.getElementById("number_2").value;
+    actual_number=parseInt(number_1)*parseInt(number_2);
+    console.log(actual_number);
+
+    question_word = "<h4>q. "+ number_1 + "X" +  number_2 + "</h4>";
+    input_box = "<br>answer:<input type='text' id='input_check'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>check</button>";
+
+    row = question_word + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number_1").value = "";
+    document.getElementById("number_2").value="";
+}
+question_turn = "player1";
+answer_turn = "player2";
+
+function check() {
+    answer = document.getElementById("input_check").value;
+    console.log("answer is " + answer);
+
+    if (answer == actual_number) {
+        if (answer_turn == "player1") {
+            player1_score += 1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        } else {
+            player2_score += 1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+    }
+    if(question_turn=="player1"){
+        question_turn="player2";
+        document.getElementById("player_question").innerHTML="question turn-"+player2_name;
+    }
+    else{
+        question_turn="player1";
+        document.getElementById("player_question").innerHTML="question turn"+player1_name;
+    }
+    if(answer_turn=="player1"){
+        answer_turn="player2";
+        document.getElementById("player_answer").innerHTML="answer turn-"+player2_name;
+    }
+    else{
+        answer_turn="player1";
+        document.getElementById("player_answer").innerHTML="answer turn"+player1_name;
+}
+document.getElementById("output").innerHTML="";
+}
